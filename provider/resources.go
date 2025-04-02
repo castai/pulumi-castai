5535c32288a63bd4d4a1ba05a277e3e3ab184301
Ilia German <iliagerman@gmail.com>
2025-04-02 15:20:20 +0000

    fix: reduce timeout for pulumi preview in GCP TypeScript example script

--- provider/resources.go
+++ provider/resources.go
@@ -55,6 +55,7 @@ func preConfigureCallback(vars resource.PropertyMap, c interface{}) error {
 // Provider returns additional overlaid schema and metadata associated with the provider.
 func Provider() tfbridge.ProviderInfo {
 	// Instantiate the Terraform provider
+	fmt.Println("Using the actual Terraform CAST AI provider")
 	p := shimv2.NewProvider(castai.Provider())
 
 	// Create a Pulumi provider mapping
