4c386dcd6321f356629137e5c5525ebd8b9686f1
germanilia <iliagerman@gmail.com>
2025-04-16 10:12:29 +0000

    feat: Add verification script for Go SDK import and update version to 0.1.28

--- scripts/test-go-import.go
+++ scripts/test-go-import.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	// This is just a test file to verify the import path
 	fmt.Println("Testing import of github.com/castai/pulumi-castai/sdk/go/castai")
-	
-	// We don't actually use the package here, just verify it can be imported
-	_ = castai.Provider
+
+	// Actually use the package to avoid the "imported and not used" error
+	// Use a function from the package
+	_ = castai.NewProvider
 }
